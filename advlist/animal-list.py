2ad07d749243f6ab23b730bde81256ef0a58e210
Kharis0 <khariszietisa@gmail.com>
2024-01-03 21:47:00 +0000

    learning about dictionaries

---
+++ advlist/animal-list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Kharis Research | KharisZ.Inc
+   List -making selections from complex lists"""
+
+
+def main():
+
+    #creat a list called animal_list
+    animal_list = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat"]
+    print(animal_list)
