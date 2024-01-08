abaaddf037a2a04fd0587c6bde288396fac72a34
Kharis0 <khariszietisa@gmail.com>
2024-01-08 19:01:19 +0000

    Learning about functions

---
+++ cfgread/cfg03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+## create file object in "r"ead mode
+file_name = input("Wat is the name of the file to load?\n")
+with open(file_name, "r") as configfile:
+    ## readlines() creates a list by reading target
+    ## file line by line
+    configlist = configfile.readlines()
+
+## file was just auto closed (no more indenting)
+
+## each item of the list now has the "\n" characters back
+print(configlist)
+print(len(configlist)) 
